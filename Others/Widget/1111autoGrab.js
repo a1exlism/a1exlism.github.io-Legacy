f1ad864c7bc9756fee74760ac1889eaf64938752
A1exlism <shengjitian@gmail.com>
2015-11-04 16:57:00 +0800

    Site updated: 2015-11-04 16:57:00

--- Others/Widget/1111autoGrab.js
+++ Others/Widget/1111autoGrab.js
@@ -1,8 +1,23 @@
 //site: https://www.tmall.com/wow/act/14700/huabei-hdc1111
+//--------Version 4----------------
+//issue:解决了和另一个脚本一样由递归导致的致命错误---
+var click1 = document.getElementById('J_LotteryBTN');
+var closeBtn = document.getElementsByClassName('ui-button-yl J_Close');
+setInterval( function(){
+	click1.click();
+	setTimeout(function(){
+		if(closeBtn[0])
+			closeBtn[0].click();
+	},50);
+},7500);			//肉眼查看了更新时间发现7.5s刚刚好2333
 
+//----------testCode:--------
+var a = document.getElementsByClassName('ui-button-yl J_Close');
+for(var i = 0;i < a.length;)
+	console.log(a[i++]);
 //--------Version 3----------------
 //issue:-----修复一直报错的问题----
-//Q:虽然没有报错的问题了,但是还是会一直卡住..不知道什么原因..
+//Q:虽然没有报错的问题了,但是脚本一段时间之后就会死..不知道什么原因..
 var click1 = document.getElementById('J_LotteryBTN');
 click1.onclick = function(){
 var closeBtn = document.getElementsByClassName('ui-button-yl J_Close')[0];
--- Others/Widget/clickAlways.js
+++ Others/Widget/clickAlways.js
@@ -2,6 +2,14 @@
 设置需要点击的地方
 id="click1111"
 
+//---------Version 2---------
+//issue:解决了因为递归而产生的性能问题,自己太傻= =--------------
+//更改后就能随便设置时间了..默认0.5s抢 可以自行设置
+var click1 = document.getElementById('click1111');
+setInterval(function(){
+		click1.click();
+	},500);
+//---------Version 1---------
 var click1 = document.getElementById('click1111');
 click1.onclick = function(){
 	setInterval(function(){
