5c47b04f9460c05976d4dcb9fd5fdba79723353f
a1exlism <shengjitian@gmail.com>
2015-11-18 16:51:38 +0800

    Site updated: 2015-11-18 16:51:36

--- Others/Codebytes/20-Array Addition.js
+++ Others/Codebytes/20-Array Addition.js
@@ -1,17 +1,10 @@
+//吗个鸡,想用递归不会用,搞了半天只会笨办法穷举2333
+//网上查了一下,用到策略:减而治之,递归
+/*function: 
+1. Array.slice(begin[,end])截取
+2. Array.unshift();	出队列
+*/
 function ArrayAddition(arr) { 
-	var max = arr[0];
-	for( var i = 1; i < arr.length ;i++){
-		if( max < arr[i] )
-			max = arr[i];
-	}
-	//------最笨的办法---------
-	var maxcp = max;	//exclude max value
-	for( var i = 0; i < arr.length; i++){
-		if( arr[i] == maxcp )
-			continue;
-		
-	}
-	// code goes here  
-	return arr; 
-	       
+	
+	return arr;
 }
