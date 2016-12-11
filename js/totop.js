5463ed8e7e22500643504332a36dc52741a19495
Quentin Smith <shengjitian@gmail.com>
2016-12-12 00:38:17 +0800

    Site updated: 2016-12-12 00:38:10

--- js/totop.js
+++ js/totop.js
@@ -1,29 +1,33 @@
-(function($) {
-    
-    // When to show the scroll link
-    // higher number = scroll link appears further down the page    
-    var upperLimit = 1000; 
-        
-    // Our scroll link element
-    var scrollElem = $('#totop');
-    
-    // Scroll to top speed
-    var scrollSpeed = 500;
-    
-    // Show and hide the scroll to top link based on scroll position    
-    scrollElem.hide();
-    $(window).scroll(function () {             
-        var scrollTop = $(document).scrollTop();        
-        if ( scrollTop > upperLimit ) {
-            $(scrollElem).stop().fadeTo(300, 1); // fade back in            
-        }else{        
-            $(scrollElem).stop().fadeTo(300, 0); // fade out
-        }
-    });
+$(function () {
 
-    // Scroll to top animation on click
-    $(scrollElem).click(function(){ 
-        $('html, body').animate({scrollTop:0}, scrollSpeed); return false; 
-    });
 
-})(jQuery);
+	// When to show the scroll link
+	// higher number = scroll link appears further down the page
+	var upperLimit = 1000;
+
+	// Our scroll link element
+	var scrollElem = $('#totop');
+
+	// Scroll to top time
+	var scrollTime = 500;
+
+	// Show and hide the scroll to top link based on scroll position
+	$(window).scroll(function () {
+		var scrollTop = $(document).scrollTop();
+		if (scrollTop > upperLimit) {
+			scrollElem.css('visibility', 'visible');
+			scrollElem.stop().fadeTo(300, 1); // fade back in
+		} else {
+			scrollElem.stop().fadeTo(300, 0); // fade out
+		}
+	})
+	;
+
+// Scroll to top animation on click
+	$(scrollElem).click(function () {
+		$('html, body').animate({scrollTop: 0}, scrollTime);
+		return false;
+
+	});
+})
+;
